0bdb3a428ed5aba8b7b3104546d093f256e7962b
Yeshaiah <windelproductions@gmail.com>
2026-06-17 21:01:42 +0100

    File updated to refernece new location for the required images.

--- javascript/IR.js
+++ javascript/IR.js
@@ -1,5 +1,8 @@
 /*This will be very worth it ;)
-If this faah sound doesn't stop, I'm gonna do something*/
+If this faah sound doesn't stop, I'm gonna do something (noob)*/ 
+
+const FolderLocation = './images/Pop-Up-Gallery'
+
 image_array = [
     'Art.jpg',
     'Art2.jpg',
@@ -12,7 +15,7 @@ function get_random_image() {
   const random_index = Math.floor(Math.random() * image_array.length);
   const selected_image = image_array[random_index];
 
-    document.querySelector('.background').style.backgroundImage = `url('./images/${selected_image}')`
+    document.querySelector('.background').style.backgroundImage = `url('${FolderLocation}/${selected_image}')`
 
 }
 
